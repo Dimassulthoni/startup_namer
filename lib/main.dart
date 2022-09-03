aa955d3758c62eae9f4a58b0de698bb6e93be0c5
Dimassulthoni <dimas.sulthoni12@gmail.com>
2022-09-03 09:31:54 +0700

    ngubah gambar

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'startup name generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('StartUp Name Generator'),
         ),
         body: Center(
           // Drop the const, and
